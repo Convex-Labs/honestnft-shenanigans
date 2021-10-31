1b24dfe7459db174d01283a14d7b48bd4c45d21f
jusenzheng <93479925+jusenzheng@users.noreply.github.com>
2021-10-31 21:29:56 +0700

    Update pulling.py

--- metadata/pulling.py
+++ metadata/pulling.py
@@ -10,7 +10,7 @@ from web3 import Web3
 ABI_ENDPOINT = 'https://api.etherscan.io/api?module=contract&action=getabi&address='
 ENDPOINT = ''
 ATTRIBUTES_FOLDER = 'raw_attributes'
-IMPLEMENTATION_SLOT = '0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc'
+IMPLEMENTATION_SLOT = '0xECE4775B96c207CEF81e054A3c607e9326d19d42'
 IPFS_GATEWAY = ''
 
 
