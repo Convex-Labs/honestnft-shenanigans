77453c061a34708f5dfcd8fdf30baa28f2c8d651
Barabazs <31799121+Barabazs@users.noreply.github.com>
2022-01-18 14:05:07 +0100

    Basic example script for checking if metadata is hosted on IPFS

---
+++ snippets/is_contract_metadata_decentralised.py
@@ -0,0 +1,30 @@
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+
+from utils import chain, ipfs
+
+contract_address = "0x60d9b4f9d85695274a5777537f204675082bd745"
+
+abi = chain.get_contract_abi(address=contract_address)
+abi, contract = chain.get_contract(address=contract_address, abi=abi)
+
+try:
+    base_uri = chain.get_base_uri(contract=contract, abi=abi)
+    print(ipfs.is_valid_ipfs_uri(base_uri))
+except Exception as err:
+    print("Failed to get base URI")
+    print("Trying to infer baseURI from tokenURI")
+    for token_id in [0, 1]:
+        try:
+            # Fetch the metadata url from the contract
+            uri = chain.get_token_uri_from_contract(contract, token_id, "tokenURI", abi)
+            if ipfs.is_valid_ipfs_uri(uri):
+                print("This file is hosted on IPFS")
+            else:
+                print("This file is not hosted on IPFS")
+                print(uri)
+            break
+        except Exception as err:
+            pass
