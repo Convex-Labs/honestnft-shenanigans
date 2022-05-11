8f7fc35a947d1b7bcbbb0a9b2982f0c3fa82db3e
Barabazs <31799121+Barabazs@users.noreply.github.com>
2022-05-11 15:48:05 +0200

    More tests for chain * use constants.py to re-use and separate constants * introduce setUp() to reduce extra network calls * Introduce blockPrinting as decorator * Add tests for most functions in chain

--- tests/helpers.py
+++ tests/helpers.py
@@ -1,5 +1,24 @@
+import os
+import sys
+
 from pathlib import Path
 
 from utils import config
+
+
+def blockPrinting(func):
+    """
+    Decorator used to block function printing to the console
+    """
+
+    def func_wrapper(*args, **kwargs):
+        with open(os.devnull, "w") as devnull:
+            old_stdout = sys.stdout
+            sys.stdout = devnull
+            func(*args, **kwargs)
+            sys.stdout = old_stdout
+
+    return func_wrapper
+
 
 TESTS_ROOT_DIR = Path(config.ROOT_DIR).joinpath("tests")
--- tests/unit/test_chain.py
+++ tests/unit/test_chain.py
@@ -1,83 +1,193 @@
-import json
-import time
 import unittest
-from pathlib import Path
 
+import web3
+
+from tests import constants
 from tests import helpers
 from utils import chain
 
 
 class TestCase(unittest.TestCase):
-    def test_get_function_signature(self):
-        CONTRACT_ABI = json.loads(
-            '[{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"approved","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"operator","type":"address"},{"indexed":false,"internalType":"bool","name":"approved","type":"bool"}],"name":"ApprovalForAll","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Transfer","type":"event"},{"inputs":[],"name":"MAX_PUBLIC_MINT","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"MAX_SUPPLY","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"PRICE_PER_TOKEN","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"PROVENANCE","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"approve","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"getApproved","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"isAllowListActive","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"operator","type":"address"}],"name":"isApprovedForAll","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"numberOfTokens","type":"uint256"}],"name":"mint","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[{"internalType":"uint8","name":"numberOfTokens","type":"uint8"}],"name":"mintAllowList","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"addr","type":"address"}],"name":"numAvailableToMint","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"ownerOf","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"renounceOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"n","type":"uint256"}],"name":"reserve","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"},{"internalType":"bytes","name":"_data","type":"bytes"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"saleIsActive","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address[]","name":"addresses","type":"address[]"},{"internalType":"uint8","name":"numAllowedToMint","type":"uint8"}],"name":"setAllowList","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"operator","type":"address"},{"internalType":"bool","name":"approved","type":"bool"}],"name":"setApprovalForAll","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"string","name":"baseURI_","type":"string"}],"name":"setBaseURI","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bool","name":"_isAllowListActive","type":"bool"}],"name":"setIsAllowListActive","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"string","name":"provenance","type":"string"}],"name":"setProvenance","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bool","name":"newState","type":"bool"}],"name":"setSaleState","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes4","name":"interfaceId","type":"bytes4"}],"name":"supportsInterface","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"index","type":"uint256"}],"name":"tokenByIndex","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"uint256","name":"index","type":"uint256"}],"name":"tokenOfOwnerByIndex","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"tokenURI","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"transferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"withdraw","outputs":[],"stateMutability":"nonpayable","type":"function"}]'
+    def setUp(self):
+        self.doodles_contract_address = constants.DOODLES_ADDRESS
+        self.doodles_abi = constants.DOODLES_ABI
+
+        self.doodles_abi, self.doodles_contract = chain.get_contract(
+            address=self.doodles_contract_address,
+            abi=self.doodles_abi,
+            blockchain="ethereum",
         )
-        self.assertIs(type(chain.get_function_signature("tokenURI", CONTRACT_ABI)), str)
+
+        self.tubbycats_contract_address = constants.TUBBYCATS_ADDRESS
+        self.tubbycats_abi = constants.TUBBYCATS_ABI
+        self.tubbycats_abi, self.tubbycats_contract = chain.get_contract(
+            address=self.tubbycats_contract_address,
+            abi=self.tubbycats_abi,
+            blockchain="ethereum",
+        )
+
+    def test_get_function_signature(self):
+
+        self.assertIs(
+            type(chain.get_function_signature("tokenURI", self.doodles_abi)), str
+        )
         self.assertEqual(
-            chain.get_function_signature("tokenURI", CONTRACT_ABI),
+            chain.get_function_signature("tokenURI", self.doodles_abi),
             "tokenURI(uint256)(string)",
         )
         self.assertEqual(
-            chain.get_function_signature("totalSupply", CONTRACT_ABI),
+            chain.get_function_signature("totalSupply", self.doodles_abi),
             "totalSupply()(uint256)",
         )
         self.assertEqual(
-            chain.get_function_signature("name", CONTRACT_ABI),
+            chain.get_function_signature("name", self.doodles_abi),
             "name()(string)",
         )
         self.assertEqual(
-            chain.get_function_signature("owner", CONTRACT_ABI),
+            chain.get_function_signature("owner", self.doodles_abi),
             "owner()(address)",
         )
         self.assertEqual(
-            chain.get_function_signature("ownerOf", CONTRACT_ABI),
+            chain.get_function_signature("ownerOf", self.doodles_abi),
             "ownerOf(uint256)(address)",
         )
         self.assertEqual(
-            chain.get_function_signature("balanceOf", CONTRACT_ABI),
+            chain.get_function_signature("balanceOf", self.doodles_abi),
             "balanceOf(address)(uint256)",
         )
         self.assertRaises(
             ValueError,
             chain.get_function_signature,
             "loremIpsum",
-            CONTRACT_ABI,
+            self.doodles_abi,
         )
 
+    def test_get_contract_abi(self):
+        contract_abi = chain.get_contract_abi(
+            self.doodles_contract_address, blockchain="ethereum"
+        )
+
+        self.assertEqual(type(contract_abi), list)
+
+        self.assertEqual(
+            contract_abi,
+            self.doodles_abi,
+        )
+
+        self.assertRaises(
+            ValueError,
+            chain.get_contract_abi,
+            address=self.doodles_contract_address,
+            blockchain="lorem_ipsum",
+        )
+
+    def test_get_contract(self):
+
+        self.assertIs(
+            type(
+                chain.get_contract(
+                    address=self.doodles_contract_address,
+                    abi=self.doodles_abi,
+                    blockchain="ethereum",
+                )
+            ),
+            tuple,
+        )
+
+    def test_get_contract_function(self):
+
+        isinstance(
+            chain.get_contract_function(
+                self.doodles_contract, "totalSupply", self.doodles_abi
+            ),
+            web3.contract.ContractFunction,
+        )
+
+    def test_get_token_uri_from_contract(self):
+
+        with self.subTest("Test with valid tokenID"):
+            token_uri = chain.get_token_uri_from_contract(
+                contract=self.doodles_contract,
+                token_id=1,
+                uri_func="tokenURI",
+                abi=self.doodles_abi,
+            )
+            self.assertEqual(type(token_uri), str)
+            # Test if the URI is modified according to our ipfs gateway?
+
+        with self.subTest("Test with invalid tokenID"):
+            self.assertRaises(
+                Exception,
+                chain.get_token_uri_from_contract,
+                contract=self.doodles_contract,
+                token_id=123456,
+                uri_func="tokenURI",
+                abi=self.doodles_abi,
+            )
+
+    def test_get_token_uri_from_contract_batch(self):
+        uri_func = chain.get_function_signature("tokenURI", self.doodles_abi)
+
+        with self.subTest("Test with all valid tokenID"):
+            token_ids = [0, 1, 2, 3, 4]
+            token_uris = chain.get_token_uri_from_contract_batch(
+                contract=self.doodles_contract,
+                token_ids=token_ids,
+                function_signature=uri_func,
+                abi=self.doodles_abi,
+                blockchain="ethereum",
+            )
+            self.assertEqual(type(token_uris), dict)
+            self.assertEqual(len(token_ids), len(token_uris))
+            self.assertEqual(type(token_uris[0]), str)
+
+        with self.subTest("Test with an invalid tokenID"):
+            token_ids = [0, 1, 234569, 3, 4]
+
+            self.assertRaises(
+                web3.exceptions.ContractLogicError,
+                chain.get_token_uri_from_contract_batch,
+                contract=self.doodles_contract,
+                token_ids=token_ids,
+                function_signature=uri_func,
+                abi=self.doodles_abi,
+                blockchain="ethereum",
+            )
+
+    @helpers.blockPrinting
+    def test_get_lower_token_id(self):
+        lower_token_id = chain.get_lower_token_id(
+            self.doodles_contract, "tokenURI", self.doodles_abi
+        )
+        self.assertEqual(type(lower_token_id), int)
+
+    def test_get_base_uri(self):
+        with self.subTest("Test without baseURI in ABI"):
+
+            self.assertRaises(
+                ValueError,
+                chain.get_base_uri,
+                contract=self.doodles_contract,
+                abi=self.doodles_abi,
+            )
+
+        with self.subTest("Test ABI with baseURI"):
+
+            base_uri = chain.get_base_uri(
+                contract=self.tubbycats_contract, abi=self.tubbycats_abi
+            )
+            self.assertEqual(type(base_uri), str)
+            self.assertEqual(
+                base_uri, "ipfs://QmeN7ZdrTGpbGoo8URqzvyiDtcgJxwoxULbQowaTGhTeZc/"
+            )
+
     def test_get_token_standard(self):
-        # Load test data
-        with open(
-            Path(helpers.TESTS_ROOT_DIR).joinpath("fixtures/test_data.json"),
-            "r",
-        ) as f:
-            self.constants = json.load(f)
-
-        for key, value in self.constants.items():
-
-            print(f"Testing {key}")
-            # Iterate over contract types (regular and proxy)
-            for _key, _value in value["test_contracts"].items():
-                # Iterate over test contracts
-                for contract_key, contract_value in _value.items():
-                    time.sleep(5)
-                    ctr_name = contract_value["name"]
-                    ctr_address = contract_value["address"]
-                    ctr_abi = contract_value["abi"]
-                    ctr_standard = contract_value["standard"]
-                    ctr_blockchain = value["chain_name"]
-
-                    _abi = chain.get_contract_abi(
-                        address=ctr_address, blockchain=ctr_blockchain
-                    )
-                    _abi, _contract = chain.get_contract(
-                        address=ctr_address, abi=_abi, blockchain=ctr_blockchain
-                    )
-
-                    # Now do the actual test for get_token_standard
-                    self.assertEqual(
-                        chain.get_token_standard(_contract),
-                        ctr_standard,
-                    )
+        token_standard = chain.get_token_standard(self.doodles_contract)
+        self.assertEqual(
+            token_standard,
+            "ERC-721",
+        )
+        self.assertEqual(type(token_standard), str)
 
 
 if __name__ == "__main__":  # pragma: no cover
