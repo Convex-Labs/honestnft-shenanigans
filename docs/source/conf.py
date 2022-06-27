ab9793a13021aede8a7e43771bfdb19871f6903f
Barabazs <31799121+Barabazs@users.noreply.github.com>
2022-06-27 15:40:57 +0200

    Add nbsphinx for notebook integration

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,7 +33,12 @@ release = "0.0.1"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx_rtd_theme", "sphinx.ext.autodoc", "sphinx_autodoc_typehints"]
+extensions = [
+    "sphinx_rtd_theme",
+    "sphinx.ext.autodoc",
+    "sphinx_autodoc_typehints",
+    "nbsphinx",
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
