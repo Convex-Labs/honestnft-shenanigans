8bcb27155cc7d141afa34e66314cc3dfb167c983
Anas <anastiour@gmail.com>
2022-07-03 22:32:01 +0100

    WIP(suspicious): define function for optimistic scraping of NFT

--- fair_drop/suspicious.py
+++ fair_drop/suspicious.py
@@ -1,7 +1,7 @@
 # See https://github.com/convex-labs/honestnft-shenanigans/issues/86 for more details
 # Given a collection of NFTs on OpenSea, detect suspicious NFTs
 from argparse import ArgumentParser
-
+import cloudscraper
 
 parser = ArgumentParser()
 parser.add_argument(
@@ -13,5 +13,11 @@ parser.add_argument(
 )
 
 args = parser.parse_args()
-print(args)
-print(args.collection_address)
+
+
+def is_nft_suspicious(nft_url):
+    scraper = cloudscraper.create_scraper()
+    res = scraper.get(nft_url)
+
+    if res.status_code == 200:
+        return res.text.find("Reported for suspicious") > 0
